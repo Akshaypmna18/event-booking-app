d0031468736fa2db4566c4b9a5d604e15ff6039d
Akshay <akshaypmnacoc@gmail.com>
2025-11-05 12:00:49 +0530

    chore: complete folder structure and add data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import { Button } from "@/components/ui/button";
-import { useStore } from "./store";
+import { Routes, Route } from "react-router";
+import { lazy, Suspense } from "react";
+
+// Lazy load pages
+const MoviesList = lazy(() => import("@/pages/MoviesList"));
+const SeatBooking = lazy(() => import("@/pages/SeatBooking"));
+const TheatreList = lazy(() => import("@/pages/TheatreList"));
+const BookingSummary = lazy(() => import("@/pages/BookingSummary"));
+
+import LoadingFallback from "@/components/LoadingFallback";
 
 export default function App() {
-  const btnText = useStore((state) => state.btnText);
-
-  return <Button onClick={() => alert("hi")}>{btnText}</Button>;
+	return (
+		<Suspense fallback={<LoadingFallback />}>
+			<Routes>
+				<Route path="/" element={<MoviesList />} />
+				<Route path="/theatre-list" element={<TheatreList />} />
+				<Route path="/seat-booking" element={<SeatBooking />} />
+				<Route path="/booking-summary" element={<BookingSummary />} />
+			</Routes>
+		</Suspense>
+	);
 }
